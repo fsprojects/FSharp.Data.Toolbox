5d6359fb9f02437ffb6719b03d96a6e92b7530d3
Tomas Petricek <tomas@tomasp.net>
2014-04-30 00:22:51 +0100

    Build tests after main (for Mono xbuild)

--- build.fsx
+++ build.fsx
@@ -80,7 +80,10 @@ Target "CleanDocs" (fun _ ->
 // Build library & test project
 
 Target "Build" (fun _ ->
-    !! (solutionFile + "*.sln")
+    !! (solutionFile + ".sln")
+    |> MSBuildRelease "" "Rebuild"
+    |> ignore
+    !! (solutionFile + ".Tests.sln")
     |> MSBuildRelease "" "Rebuild"
     |> ignore
 )
