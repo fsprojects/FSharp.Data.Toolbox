4a5bde0f252313e291a8625756aa145a6de8304b
Tomas Petricek <info@tomasp.net>
2014-04-30 00:32:25 +0100

    Undo unnecessary change

--- src/FSharp.Data.Toolbox.Twitter/Twitter.fs
+++ src/FSharp.Data.Toolbox.Twitter/Twitter.fs
@@ -233,7 +233,7 @@ module WebRequestExtensions =
 // ----------------------------------------------------------------------------------------------
 
 module internal TwitterTypes = 
-  type Tweet = JsonProvider<"json/stream.json", EmbeddedResource="FSharp.Data.Toolbox.Twitter,stream.json", SampleIsList=true>
+  type Tweet = JsonProvider<"json/stream.json", SampleIsList=true, EmbeddedResource="FSharp.Data.Toolbox.Twitter,stream.json">
   type TimeLine = JsonProvider<"json/timeline.json", EmbeddedResource="FSharp.Data.Toolbox.Twitter,timeline.json">
   type SearchTweets = JsonProvider<"json/search_tweets.json", EmbeddedResource="FSharp.Data.Toolbox.Twitter,search_tweets.json">
   type IdsList = JsonProvider<"json/idslist.json", EmbeddedResource="FSharp.Data.Toolbox.Twitter,idslist.json">
