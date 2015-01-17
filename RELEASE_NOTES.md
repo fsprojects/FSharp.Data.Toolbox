e4dd1a795fa0311abe3285d21dbfa1099bfe88ff
Tomas Petricek <tomas@tomasp.net>
2015-01-17 04:43:16 +0100

    Bump version to 0.6

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -16,3 +16,6 @@
 #### 0.5 - January 6 2015
 * Added geo support
 * Fixed FSharp.Data dependency
+
+#### 0.6 - January 17 2015
+* Added function to access timeline mentions (Thanks @teramonagi!)
--- src/FSharp.Data.Toolbox.Twitter/AssemblyInfo.fs
+++ src/FSharp.Data.Toolbox.Twitter/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Data.Toolbox.Twitter")>]
 [<assembly: AssemblyProductAttribute("FSharp.Data.Toolbox")>]
 [<assembly: AssemblyDescriptionAttribute("F# Data-based library for various data access APIs.")>]
-[<assembly: AssemblyVersionAttribute("0.5")>]
-[<assembly: AssemblyFileVersionAttribute("0.5")>]
+[<assembly: AssemblyVersionAttribute("0.6")>]
+[<assembly: AssemblyFileVersionAttribute("0.6")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.5"
+    let [<Literal>] Version = "0.6"
