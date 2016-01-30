f881178d36e6af49ab6561cb5b8818ab3c8f9e75
Tomas Petricek <tomas@tomasp.net>
2016-01-30 17:01:06 +0000

    Update build to work with new FAKE

--- build.sh
+++ build.sh
@@ -1,38 +1,40 @@
-#!/bin/bash
-if test "$OS" = "Windows_NT"
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+cd `dirname $0`
+
+PAKET_BOOTSTRAPPER_EXE=.paket/paket.bootstrapper.exe
+PAKET_EXE=.paket/paket.exe
+FAKE_EXE=packages/build/FAKE/tools/FAKE.exe
+
+FSIARGS=""
+OS=${OS:-"unknown"}
+if [[ "$OS" != "Windows_NT" ]]
 then
-  # use .Net
-
-  .paket/paket.bootstrapper.exe
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
-  fi
-
-  .paket/paket.exe restore
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
-  fi
-  
-  [ ! -e build.fsx ] && .paket/paket.exe update
-  [ ! -e build.fsx ] && packages/FAKE/tools/FAKE.exe init.fsx
-  packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
-else
-  # use mono
-  mono .paket/paket.bootstrapper.exe
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
-  fi
-
-  mono .paket/paket.exe restore
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
-  fi
-
-  [ ! -e build.fsx ] && mono .paket/paket.exe update
-  [ ! -e build.fsx ] && mono packages/FAKE/tools/FAKE.exe init.fsx
-  mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
+  FSIARGS="--fsiargs -d:MONO"
 fi
+
+function run() {
+  if [[ "$OS" != "Windows_NT" ]]
+  then
+    mono "$@"
+  else
+    "$@"
+  fi
+}
+
+run $PAKET_BOOTSTRAPPER_EXE
+
+if [[ "$OS" != "Windows_NT" ]] &&
+       [ ! -e ~/.config/.mono/certs ]
+then
+  mozroots --import --sync --quiet
+fi
+
+run $PAKET_EXE restore
+
+[ ! -e build.fsx ] && run $PAKET_EXE update
+[ ! -e build.fsx ] && run $FAKE_EXE init.fsx
+run $FAKE_EXE "$@" $FSIARGS build.fsx
