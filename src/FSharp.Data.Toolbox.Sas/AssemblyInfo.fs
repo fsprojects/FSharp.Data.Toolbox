f0b3298bc77eddaa3820a6fa82aac93e87acb2b5
Tomas Petricek <tomas@tomasp.net>
2016-05-04 21:09:04 +0100

    Bump version to 0.15

--- src/FSharp.Data.Toolbox.Sas/AssemblyInfo.fs
+++ src/FSharp.Data.Toolbox.Sas/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Data.Toolbox.Sas")>]
 [<assembly: AssemblyProductAttribute("FSharp.Data.Toolbox")>]
 [<assembly: AssemblyDescriptionAttribute("F# Data-based library for various data access APIs.")>]
-[<assembly: AssemblyVersionAttribute("0.14")>]
-[<assembly: AssemblyFileVersionAttribute("0.14")>]
+[<assembly: AssemblyVersionAttribute("0.15")>]
+[<assembly: AssemblyFileVersionAttribute("0.15")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.14"
-    let [<Literal>] InformationalVersion = "0.14"
+    let [<Literal>] Version = "0.15"
+    let [<Literal>] InformationalVersion = "0.15"
