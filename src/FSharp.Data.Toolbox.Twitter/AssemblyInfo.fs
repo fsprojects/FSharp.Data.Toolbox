2e40d92a1e2849f3076ad5052cf14e7a641dcd5c
Tomas Petricek <tomas@tomasp.net>
2016-05-09 16:23:48 +0100

    Bump version to 0.16

--- src/FSharp.Data.Toolbox.Twitter/AssemblyInfo.fs
+++ src/FSharp.Data.Toolbox.Twitter/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Data.Toolbox.Twitter")>]
 [<assembly: AssemblyProductAttribute("FSharp.Data.Toolbox")>]
 [<assembly: AssemblyDescriptionAttribute("F# Data-based library for various data access APIs.")>]
-[<assembly: AssemblyVersionAttribute("0.15")>]
-[<assembly: AssemblyFileVersionAttribute("0.15")>]
+[<assembly: AssemblyVersionAttribute("0.16")>]
+[<assembly: AssemblyFileVersionAttribute("0.16")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.15"
-    let [<Literal>] InformationalVersion = "0.15"
+    let [<Literal>] Version = "0.16"
+    let [<Literal>] InformationalVersion = "0.16"
