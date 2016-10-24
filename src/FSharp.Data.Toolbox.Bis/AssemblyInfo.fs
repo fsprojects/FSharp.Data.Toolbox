32cfc799b0962cd08c0877f46118d4486be2b294
Micic, Darko <darko.micic@bis.org>
2016-10-24 22:39:22 +0200

    Initial check-in

---
+++ src/FSharp.Data.Toolbox.Bis/AssemblyInfo.fs
@@ -0,0 +1,14 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FSharp.Data.Toolbox.Bis")>]
+[<assembly: AssemblyProductAttribute("FSharp.Data.Toolbox")>]
+[<assembly: AssemblyDescriptionAttribute("F# Data-based library for various data access APIs.")>]
+[<assembly: AssemblyVersionAttribute("0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.1")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.1"
+    let [<Literal>] InformationalVersion = "0.1"
+    
