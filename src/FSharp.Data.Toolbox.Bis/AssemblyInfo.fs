46d7bbddbfc4ec2df5c8f06672a4225c63e0dfaa
Micic, Darko <darko.micic@bis.org>
2016-11-01 21:37:44 +0100

    Added nuspec. Updated build.fsx.

--- src/FSharp.Data.Toolbox.Bis/AssemblyInfo.fs
+++ src/FSharp.Data.Toolbox.Bis/AssemblyInfo.fs
@@ -4,11 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Data.Toolbox.Bis")>]
 [<assembly: AssemblyProductAttribute("FSharp.Data.Toolbox")>]
 [<assembly: AssemblyDescriptionAttribute("F# Data-based library for various data access APIs.")>]
-[<assembly: AssemblyVersionAttribute("0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.1")>]
+[<assembly: AssemblyVersionAttribute("0.17")>]
+[<assembly: AssemblyFileVersionAttribute("0.17")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1"
-    let [<Literal>] InformationalVersion = "0.1"
-    
+    let [<Literal>] Version = "0.17"
+    let [<Literal>] InformationalVersion = "0.17"
