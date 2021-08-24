29a99077698b2411288ba35823bbdd743fc6c52f
nhirschey <nhirschey@users.noreply.github.com>
2021-08-24 14:11:02 +0100

    More clear #r nuget explanation

--- docs/TwitterProvider.fsx
+++ docs/TwitterProvider.fsx
@@ -20,9 +20,7 @@ to its API. You have to register your application at
 [Twitter Apps](https://apps.twitter.com/). 
 After registration, Twitter provides API key and API secret to authenticate the application.
 
-If you are using F# Interactive, you first need to reference the Twitter type
-provider assembly. Assuming you obtain the package from NuGet and the assembly
-is in `packages`, this would look as follows:
+If you are using F# Interactive you can reference the nuget package directly:
 *)
 
 (*** condition: prepare ***)
@@ -36,8 +34,15 @@ is in `packages`, this would look as follows:
 #r "nuget: FSharp.Data.Toolbox.Twitter,{{fsdocs-package-version}}"
 #endif // IPYNB
 
-(*** do-not-eval ***)
-#r "nuget: FSharp.Data.Toolbox.Twitter,{{fsdocs-package-version}}"
+(**
+
+You can use FSharp.Data.Toolbox in [dotnet interactive](https://github.com/dotnet/interactive) notebooks in [Visual Studio Code](https://code.visualstudio.com/) or [Jupyter](https://jupyter.org/), or in F# scripts (`.fsx` files), by referencing the package as follows:
+
+    // Use one of the following three lines
+    #r "nuget: FSharp.Data.Toolbox.Twitter"                            // Use the latest version
+    #r "nuget: FSharp.Data.Toolbox.Twitter,{{fsdocs-package-version}}" // Use a specific version
+
+*)
 
 #r "nuget: FSharp.Data"
 
